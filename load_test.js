7d6815fb3b063688876a664d3e78d8ba9662d130
Sergey Kuchmistov <sergey.kuchmistov@gmail.com>
2012-04-16 00:19:38 +0300

    Add js startup file for nodeload

---
+++ load_test.js
@@ -0,0 +1,17 @@
+var nl = require('nodeload')
+  .setMonitorIntervalMs(500)
+  .setAjaxRefreshIntervalMs(500);
+
+var loadtest = nl.run({
+  name: "First test",
+  numUsers: 100,
+  host: 'localhost',
+  port: 8888,
+  timeLimit: 30,
+  targetRps: 500,
+  stats: ['latency','result-codes', 'concurrency'],
+  methods: 'GET',
+  path: '/json/products/'
+});
+
+loadtest.on('end', function() { console.log('tests done'); });
